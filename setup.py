042bd9ee025c0b9e93a7a61c8ab19683308d543d
Esteban Madrigal <emadri1@cam.wal-mart.com>
2018-11-01 21:48:19 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='proyecto_tox',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='emadri1',
+    author_email='',
+    description=''
+)
---
+++ test_ejemplo.py
@@ -0,0 +1,6 @@
+def test_division():
+    a = 10
+    b = 3
+    c = a//b
+
+    assert 3 == c, 'division entera exitosa'
